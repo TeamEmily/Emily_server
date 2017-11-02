ab3cdb66c21b3bf380427695343ac9e2c8a5b47d
yuda1124 <yuda1124@naver.com>
2017-11-02 11:46:55 +0900

    add epl query function

--- api/views.py
+++ api/views.py
@@ -2,11 +2,17 @@ from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from src.intentAnalyzer.intentAnalyzer import IntentAnalyzer
+from rest_framework.renderers import JSONRenderer
+from epl.models import Teamrecord
+from epl.serializers import TeamRecordSerializer
+
 intentAnalyzer = IntentAnalyzer()
 
 class getIntent(APIView):
 
     def get(self, request, format=None):
+        teamrecord = Teamrecord.objects.all()
+        serializer = TeamRecordSerializer(teamrecord, many=True)
         string = request.GET.get('str')
         intent, intent_num = intentAnalyzer.analyzeIntent(string)
         if(intent_num == -1):
@@ -15,4 +21,26 @@ class getIntent(APIView):
         if ("error" in params.keys()):
             return Response({"error": params["error"]})
         else:
-            return Response({"intent": intent, "params": params})
+            data = self.getResult(params, intent_num)
+            return Response({"intent": intent, "data": data})
+
+    def getResult(self, params, intent):
+        print(intent)
+        funcMap = {
+            2: self.getRecord(params)
+        }
+        return funcMap.get(intent)
+
+    def getRecord(self, params, format=None):
+        data = []
+        for param in params["FC"]:
+            if len(param) == "리그":
+                teamrecord = Teamrecord.objects.all()
+                serializer = TeamRecordSerializer(teamrecord, many=True)
+            else:
+                print(param)
+                teamrecord = Teamrecord.objects.get(pk=param)
+                serializer = TeamRecordSerializer(teamrecord)
+                print(serializer.data)
+            data.append(serializer.data)
+        return data
---
+++ epl/serializers.py
@@ -0,0 +1,14 @@
+from django.forms import widgets
+from rest_framework import serializers
+from epl.models import Teams, Stats, Teamrecord, Players
+
+
+class TeamsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teams
+        fields = ('team_id', 'team_name', 'team_stadium', 'team_manager', 'team_nickname')
+
+class TeamRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teamrecord
+        fields = ('teamname', 'winscore', 'playedgames', 'winnum', 'losenum', 'drawnum', 'goalscore', 'losepoint', 'difgoallose')
